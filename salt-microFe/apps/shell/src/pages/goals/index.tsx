57d435505869f104f342d2df46387f5949256a21
kwangyoulsagong <sgky0511@naver.com>
2025-01-11 18:00:13 +0900

    feat: 상태관리 프롭스 전달 mfe

--- salt-microFe/apps/shell/src/pages/goals/index.tsx
+++ salt-microFe/apps/shell/src/pages/goals/index.tsx
@@ -1,10 +1,12 @@
+import { useAppSelector } from "@/hooks/redux/hooks";
 import { lazy, Suspense } from "react";
 
 const Goals = lazy(() => import("goals/App"));
 const GoalPage = () => {
+  const profile = useAppSelector((state) => state.auth);
   return (
     <Suspense fallback={<div>로딩 중...</div>}>
-      <Goals />
+      <Goals nickname={profile.user?.nickname} />
     </Suspense>
   );
 };
--- salt-microFe/apps/shell/src/pages/index.tsx
+++ salt-microFe/apps/shell/src/pages/index.tsx
@@ -1,14 +1,28 @@
-import React, { Suspense, lazy, useState, useEffect } from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import { themeClass, container } from "@repo/ui/styles";
 import { Button } from "@repo/ui/button";
+import { useAppDispatch, useAppSelector } from "@/hooks/redux/hooks";
+import { setUser } from "@/store/redux/features/auth/authSlice";
 const Game = lazy(() => import("game/App"));
 const Social = lazy(() => import("social/App"));
 
 export default function Home({ children }: { children?: React.ReactNode }) {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    const userData = {
+      id: 1,
+      nickname: "광열",
+      email: "user@example.com",
+    }; // 예시 데이터
+
+    dispatch(setUser(userData)); // setUser 액션 호출
+  }, [dispatch]);
+  const profile = useAppSelector((state) => state.auth);
   return (
     <div className={`${themeClass} ${container}`}>
       <main>
         <h1>쉘 (Shell) 애플리케이션</h1>
+        <h1 style={{ color: "#ffffff" }}>{profile.user?.nickname}</h1>
 
         <Suspense fallback={<div>로딩 중...</div>}>
           <Button variant="primary" routes="/goals">
