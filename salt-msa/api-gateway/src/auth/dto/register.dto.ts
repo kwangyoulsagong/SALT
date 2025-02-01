89bfe7d9392eb24f0f76ce7e16c68340c3b65840
kwangyoulsagong <sgky0511@naver.com>
2025-02-01 11:30:30 +0900

    feat: api-gateway-dto

---
+++ salt-msa/api-gateway/src/auth/dto/register.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @MinLength(2)
+  nickname: string;
+}
